ecb9fa21722070c16e32bc9816070bbec665d403
Rockelle Gardner <rockellegardner@gmail.com>
2024-03-29 10:18:19 -0400

    first commit

---
+++ SBA-318-College-Coursework-ExpressJS-main/data/collegecourses.js
@@ -0,0 +1,46 @@
+const collegeCourses = [
+    {
+        id:1001,
+        name: "Introduction to Psychology",
+        department: "Psychology",
+        professor: "Dr. Amanda Smith",
+        credits: 3,
+        description: "An overview of basic principles and concepts in psychology, covering topics such as human behavior, cognition, development, and mental processes."
+    },
+    {
+        id:1002,
+        name: "Introduction to Computer Science",
+        department: "Computer Science",
+        professor: "Dr. John Doe",
+        credits: 4,
+        description: "Fundamental concepts of computer science including programming fundamentals, algorithms, data structures, and problem-solving techniques."
+    },
+    {
+        id:1003,
+        name: "World History: From Ancient Times to the Present",
+        department: "History",
+        professor: "Prof. Emily Johnson",
+        credits: 3,
+        description: "A survey of world history from ancient civilizations to contemporary societies, exploring major events, cultural developments, and global interactions."
+    },
+    {
+        id:1004,
+        name: "Principles of Microeconomics",
+        department: "Economics",
+        professor: "Dr. Michael Brown",
+        credits: 3,
+        description: "An introduction to microeconomic theory, covering topics such as supply and demand, market structures, consumer behavior, and economic decision-making."
+    },
+    {
+        id:1005,
+        name: "Introduction to Literature",
+        department: "English",
+        professor: "Dr. Sarah Adams",
+        credits: 3,
+        description: "Exploration of literary genres, themes, and techniques through the study of various literary works, including novels, short stories, poetry, and drama."
+    }
+];
+
+// console.log(collegeCourses);
+
+module.exports = collegeCourses; 
---
+++ SBA-318-College-Coursework-ExpressJS-main/index.js
@@ -0,0 +1,111 @@
+const Joi = require('joi'); //input validator module
+const express = require('express') //LOAD THE EXPRESS MODULE
+const app = express(); //APPLICATION
+const port = 3000; 
+const courses = require('./data/collegecourses');
+const collegeCourses = require('./data/collegecourses');
+
+
+
+// PARSER
+app.use(express.json());
+
+
+// HTTP METHODS
+app.get('/', (req,res)=> { 
+    res.send('SBA EXPRESS COLLEGE COURSE DEMO'); 
+});
+
+// ALL COURSES END POINT
+app.get('/api/collegecourses', (req, res) => {
+    // console.log(collegeCourses)
+    try {
+        res.json(collegeCourses); // Respond with JSON data of all college courses
+    } catch (error) {
+        console.error('Error fetching college courses:', error);
+        res.status(500).send('Internal Server Error'); // If error occurs, return 500 status
+    }
+});
+
+// GET A SINGLE COURSE 
+app.get('/api/collegecourses/:id', (req, res, next) => {
+    try {
+        // console.log(req.params.id)
+        const collegeCourse = collegeCourses.find(collegeCourse => collegeCourse.id == req.params.id);
+    //    console.log(`final id`, collegeCourse)
+        if(!collegeCourse) throw Error
+       res.status(200).json(collegeCourse)
+    } catch (error) {
+        res.status(400).json({error: `Course ID not found`})
+    }
+});
+
+
+
+// POST=========================================
+// app.post()
+app.post('/api/collegecourses/:id', (req,res)=>{ 
+    const course = { 
+        id : courses.length + 1, 
+        name: req.body.name,
+        department: req.body.department, 
+        professor: req.body.professor, 
+        credits: req.body.credits, 
+        description: req.body.description
+    }; 
+    collegeCourses.push(course);
+    res.send(course);
+});
+
+// app.put()
+app.put('/api/collegecourses/:id', (req,res)=> { 
+    try {
+        // console.log(req.params.id)
+        const collegeCourse = collegeCourses.find(collegeCourse => collegeCourse.id == req.params.id);
+    //    console.log(`final id`, collegeCourse)
+        if(!collegeCourse) throw Error
+       res.status(200).json(collegeCourse)
+    } catch (error) {
+        res.status(400).json({error: `Course ID not found`})
+
+        const schema = { 
+            name: Joi.string()
+        }
+        const result = Joi.validate(req.body, schema); 
+        if (result.error){  
+            res.status(404).send(result.error); 
+            return
+        }
+        collegeCourse.name= req.body.name; 
+        res.send(collegeCourse);
+    } 
+})
+
+function validateCourse(collegeCourse){ 
+    const schema = { 
+        name: Joi.string()
+    }
+    return Joi.validate(collegeCourse,schema);
+}
+
+// app.delete()
+app.delete('/api/collegecourses/:id', (req,res)=> { 
+    try {
+        // console.log(req.params.id)
+        console.log(`college course:`,collegeCourse)
+        console.log(`college courses:`, collegeCourses)
+        const collegeCourse = collegeCourses.find(collegeCourse => collegeCourse.id == req.params.id);
+    //    console.log(`final id`, collegeCourse)
+        if(!collegeCourse)res.status(404).send(`The course with the given ID is not found`)
+    } catch (error) {
+        res.status(400).json({error: `Course ID not found`})
+        const index = collegeCourses.indexOf(collegeCourses); 
+        collegeCourses.splice(index,1); 
+
+        res.send(collegeCourses); 
+    }
+
+})
+app.listen(port, ()=>{ 
+    console.log(`server is running on port ${port}`)
+});
